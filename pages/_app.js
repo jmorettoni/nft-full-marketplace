9a515100809208cf0e9d5fc1a15422d9ec76815a
jmorettoni <jmorettoni@gmail.com>
2022-03-01 22:48:52 -0300

    ajuste home

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,11 @@ import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <div className="currentNetwork"><p style={{width:"250px",float:"right", textAlign:"right" , marginRight:"20px"}}><small>NetWork : Mumbai Testnet<br/> (Matic)</small></p></div>
+      <div className="currentNetwork"><p style={{width:"250px", position:"absolute" , right:"0px", textAlign:"right" , marginRight:"20px"}}><small>NetWork : Mumbai Testnet<br/> (Matic)</small></p></div>
       <nav className="border-b p-6">
+        
         <p style={{textAlign:'center', width:"100%"}} className="text-4xl font-bold">NFT Marketplace</p>
-        <p style={{textAlign:'center', width:"100%" , fontSize:"0.5em"}}  ><small>Multi chain network ()</small></p> 
-
-
+    
         <div className="flex mt-4">
           <Link href="/">
             <a className="mr-4 text-pink-500">
